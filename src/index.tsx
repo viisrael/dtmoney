9acd1f3928f2d946dd60b3c012f254dc0fc95809
Vially Israel <viallyisrael@hotmail.com>
2022-06-14 20:37:09 -0300

    Cap 2 - unit 4 completed

--- src/index.tsx
+++ src/index.tsx
@@ -10,12 +10,42 @@ createServer({
     transaction: Model,
   },
 
+  seeds(server){
+    server.db.loadData({
+      transactions:[
+        {
+          id: 1,
+          title: 'Freelancer de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-02-12 09:00:00')
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1000,
+          createdAt: new Date('2021-02-12 09:00:00')
+        },
+        {
+          id: 3,
+          title: 'Freelancer de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-02-12 09:00:00')
+        },
+    ]
+    });
+  },
 
   routes() {
     this.namespace= 'api';
 
     this.get('/transactions', ()=>{
-      return this.schema.all('transcation');
+      return this.schema.all('transaction');
     });
 
     this.post('/transaction', (schema, request) =>{
